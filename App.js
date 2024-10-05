74a5f5f94472dfc971dbd24274b20353061a7011
Aiting Zhou <zhou0248@algonquinlive.com>
2024-10-05 17:51:45 -0400

    complete individual features: avatar-dispaly,fetch10users, fetchOneUser, Fab

--- App.js
+++ App.js
@@ -1,20 +1,140 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState, useEffect, useCallback } from "react";
+import Icon from "react-native-vector-icons/FontAwesome";
+import axios from "axios";
+import {
+  Platform,
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  RefreshControl,
+  Text,
+  Image,
+  View,
+  Pressable,
+} from "react-native";
 
 export default function App() {
+  const [refreshing, setRefreshing] = useState(false);
+  const [userData, setUserData] = useState([]);
+
+  const get10NewUsers = async () => {
+    await axios
+      .get(
+        "https://random-data-api.com/api/v2/users?size=10&response_type=json"
+      )
+      .then((response) => {
+        setUserData([...userData, ...response.data]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const getOneUser = async () => {
+    await axios
+      .get("https://random-data-api.com/api/v2/users?size=1&response_type=json")
+      .then((response) => {
+        setUserData([response.data, ...userData]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    get10NewUsers();
+    setRefreshing(false);
+  }, []);
+
+  const onPress = useCallback(() => {
+    getOneUser();
+  }, []);
+
+  useEffect(() => {
+    get10NewUsers();
+  }, []);
+
+  const renderItem = ({ item }) =>
+    Platform.OS === "ios" ? (
+      <View style={styles.user}>
+        <View style={styles.userName}>
+          <Text style={styles.name}>{item.first_name}</Text>
+          <Text style={styles.name}>{item.last_name}</Text>
+        </View>
+        <Image source={{ uri: item.avatar }} style={styles.image} />
+      </View>
+    ) : (
+      <View style={styles.user}>
+        <Image source={{ uri: item.avatar }} style={styles.image} />
+        <View style={styles.userName}>
+          <Text style={styles.name}>{item.first_name}</Text>
+          <Text style={styles.name}>{item.last_name}</Text>
+        </View>
+      </View>
+    );
+
+  const keyExtractor = (item) => item.uid;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={userData}
+        renderItem={renderItem}
+        keyExtractor={keyExtractor}
+        style={styles.list}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
+      <Pressable style={styles.btn} onPress={onPress}>
+        <Icon name="user-plus" size={25} color={"white"} />
+      </Pressable>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: Platform.OS === "ios" ? 0 : 50,
+  },
+  image: {
+    backgroundColor: "#aaa",
+    width: 80,
+    height: 80,
+    borderRadius: 15,
+  },
+  list: {
+    width: "100%",
+    marginTop: 20,
+  },
+  user: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    alignItems: "center",
+    margin: 5,
+    padding: 20,
+    borderWidth: 2,
+    borderColor: "#99d",
+    borderRadius: 15,
+  },
+  userName: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: Platform.OS === "ios" ? "flex-start" : "flex-end",
+    fontSize: 24,
+  },
+  name: {
+    fontSize: 18,
+  },
+  btn: {
+    position: "absolute",
+    bottom: 40,
+    right: 20,
+    backgroundColor: "#449",
+    padding: 20,
+    borderRadius: 45,
+    margin: 10,
   },
 });
